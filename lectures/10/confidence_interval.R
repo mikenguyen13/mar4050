ac9d81d584036f54085e606eba53c37a11f2a7b5
Mike Nguyen <57576563+mikenguyen13@users.noreply.github.com>
2021-10-27 10:22:06 -0500

    update

--- lectures/10/confidence_interval.R
+++ lectures/10/confidence_interval.R
@@ -2,17 +2,19 @@
 # mean CI
 
 n.draw = 100
-mu = 9
-n = 500
-SD = 1.5
+mu = 12 # the true mean 
+n = 100000
+SD = 1.5 # the true standard deviation 
+
 
 draws = matrix(rnorm(n.draw * n , mu, SD), n)
 
-get.conf.int = function(x) t.test(x)$conf.int
+get.conf.int = function(x) t.test(x)$conf.int # we want to get the confidence interval for each sample
 conf.int = apply(draws, 2, get.conf.int)
 sum(conf.int[1, ] <= mu & conf.int[2, ] >= mu)
 
 plot(
+  
   range(conf.int),
   c(0, 1 + n.draw),
   type = "n",
@@ -20,6 +22,6 @@ plot(
   ylab = "sample run"
 )
 for (i in 1:n.draw) lines(conf.int[, i], rep(i, 2), lwd = 2)
-abline(v = 9, lwd = 2, lty = 2)
+abline(v = 12, lwd = 2, lty = 2)
 
 
